c80b138acf8b837070a66b2da44d383895716a83
julianorsj <julianorsj@gmail.com>
2021-02-19 23:52:29 -0300

    novo commit

--- inicio.php
+++ inicio.php
@@ -2,5 +2,6 @@
 	
 	echo "<br/><br/>Welcome to PHP Developmente <p style=\"color:red;font-size:60px;font-weight:bold\">Juliano</p>";
 	echo "<br/>Será um periodo de grandes feitos.";
+	echo "<br/><p>Tudo depende do esforço pessoal.</p>";
 
 ?>
